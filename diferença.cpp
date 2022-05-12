d46085f138a4dd5927661fffc13436cf80b69e14
bertin18 <101373951+bertin18@users.noreply.github.com>
2022-05-12 03:20:55 -0300

    Create diferença.cpp
    Leia quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).

---
+++ diferença.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    int A, B, C, D, di;
+    cin>>A>>B>>C>>D;
+    di=(A*B-C*D);
+    cout<<"DIFERENCA = "<<di<<endl;
+    return 0;
+}
