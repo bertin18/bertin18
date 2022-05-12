c006d0708f07e7be17fd2bf9eddfb9f6eae654fa
bertin18 <101373951+bertin18@users.noreply.github.com>
2022-05-12 14:25:39 -0300

    Create consumo.cpp
    Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).

---
+++ consumo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+ 
+int main() {
+    int X;
+    double Y,C;
+    cin>>X>>Y;
+    C=X/Y;
+    cout<<fixed<<setprecision(3);
+    cout<<C<<" km/l"<<endl;
+    return 0;
+}
